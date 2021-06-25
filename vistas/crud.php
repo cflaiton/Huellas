ed58ce65a6070b01ae85d39abfc53e06b05a76aa
Fredy_M <fredymarcelo@wposs.com>
2021-06-24 19:39:07 -0500

    Correcion

--- vistas/crud.php
+++ vistas/crud.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-  if(!isset($_SESSION["Correo"] ) && !isset($_SESSION["id"] ) && !isset($_SESSION["Rol"] )&& !isset($_SESSION["Nombre"] )){
-    header("Location: ../vistas/login.php");
-    die();
-  }
+// session_start();
+//   if(!isset($_SESSION["Correo"] ) && !isset($_SESSION["id"] ) && !isset($_SESSION["Rol"] )&& !isset($_SESSION["Nombre"] )){
+//     header("Location: ../vistas/login.php");
+//     die();
+//   }
 ?>
    
     <?php 
--- vistas/home.php
+++ vistas/home.php
@@ -1,8 +1,2 @@
-<?php
-session_start();
-  if(!isset($_SESSION["Correo"] ) && !isset($_SESSION["id"] ) && !isset($_SESSION["Rol"] )&& !isset($_SESSION["Nombre"] )){
-    header("Location: ../vistas/login.php");
-    die();
-  }
-?>
+
 <h1>Bienvenido <?php echo $_SESSION ["Nombre"]; ?></h1>
